4342f7efc545108b575424cc0876ccf5ea1f2125
hosie <jhosie@uk.ibm.com>
2015-08-03 07:55:17 +0100

    update question generator to use the topic registry

--- mathQuestions/components/QuestionGenerator.js
+++ mathQuestions/components/QuestionGenerator.js
@@ -1,54 +1,31 @@
 'use strict';
 
-angular.module('GreatMath.question-generator', [])
-.factory('questionGenerator', [function() {
+angular.module('GreatMath.question-generator', ['GreatMath.topic-registry'])
+.factory('questionGenerator', ['topicRegistry',function(topicRegistry) {
   return{
     defaultGeneratorFunction:null,
-    topics:[],
-    questionGeneratorFunctions:{},
-    otherwise:function(defaultGeneratorFunction){
+    setDefault:function(defaultGeneratorFunction){
       this.defaultGeneratorFunction=defaultGeneratorFunction;
     },
     generate:function(questionSpec,callback){
       var generatorFunction;
-      if(undefined!=this.questionGeneratorFunctions[questionSpec.topicClass]){
-        generatorFunction = this.questionGeneratorFunctions[questionSpec.topicClass][questionSpec.topicId];
-      }
-        
-      if(undefined!=generatorFunction){
-        generatorFunction(callback,questionSpec);
-      }else{
-        if(null!=this.defaultGeneratorFunction){
-          this.defaultGeneratorFunction(questionSpec,callback);                  
-        }else{
-          setTimeout(function(){
-            callback("no function registered for topic " + questionSpec.topicId + " and no default registered");
-          },0);
-        }        
-      }
-      
-    
-      
-    },
-    when:function(questionSpec,generatorFunction){
-      this.topics.push({
-        id:questionSpec.topicId,
-        description:questionSpec.topicDescription
+      var topicId = questionSpec.topicId;
+      var self=this;
+      topicRegistry.getTopics({class:questionSpec.topicClass},function(err,topics){
+        if(err==null){
+          generatorFunction = topics[topicId-1].generateQuestion;//TODO this -1 business means that we know something about how the topicRegistry  allocates IDs. This is wrong
+          generatorFunction(callback,questionSpec);
+        }else if(err==topicRegistry.ERR_UNKNOWN_ID){
+          if(null!=self.defaultGeneratorFunction){
+            self.defaultGeneratorFunction(questionSpec,callback);                  
+          }else{
+            setTimeout(function(){
+              callback("no function registered for topic " + questionSpec.topicId + " and no default registered");
+            },0);
+          }
+        }
       });
-      if(undefined==this.questionGeneratorFunctions[questionSpec.topicClass]){
-        this.questionGeneratorFunctions[questionSpec.topicClass]={};
-      }
-      this.questionGeneratorFunctions[questionSpec.topicClass][questionSpec.topicId]=generatorFunction;
-      return this;
-    },
-    getTopics:function(callback){
-      //needs to be asynchronous in case it is called from within a digest or apply and the callback wants to run apply
-      var self = this;
-      setTimeout(function(){
-        callback(self.topics);  
-      },0);
-      
-    }
-    
+        
+    }    
   };
 }]);
--- mathQuestions/components/QuestionGenerator_test.js
+++ mathQuestions/components/QuestionGenerator_test.js
@@ -1,237 +1,67 @@
 'use strict';
 
 describe('GreatMath.question-generator module', function() {
+  
+  var mockTopicRegistry={
+    
+    getTopics:function(filter,callback){
+      
+    }
+  };
+  
   var questionGenerator;
 
   beforeEach(module('GreatMath.question-generator'));
+
+  beforeEach(module(function($provide){
+    $provide.factory('topicRegistry',function(){
+      //return a mock topicRegistry 
+      return mockTopicRegistry;
+    });            
+  }));
   
   beforeEach(inject(function(_questionGenerator_){
     questionGenerator=_questionGenerator_;    
   }));
   
-  //TODO - refactor the registry and generator out to separate modules? So that we can test the behaviour of each concern separately.
-  // topicList belongs in registry?
-  describe('registry',function(){
-    it('Register a function for a topic id',function(done){
-      var questionSpec = {
-        topicClass : "mentalStrategies",
-        topicId    : 1
-      };
-      questionGenerator.when(
-        {
-          topicClass : "mentalStrategies",
-          topicId    : 1
-        },function(callback){
-          callback(null,"question1");
-        }
-      );
-      questionGenerator.generate(
-        {
-          topicClass : "mentalStrategies",
-          topicId    : 1
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question1");
-          done();
-        }
-      );    
-    });
-    
-    it('Calls correct function for each topic',function(done){
-      var callbacksRecorded=[false,false,false];
-      var recordCallback=function(index){
-        callbacksRecorded[index]=true;
-        if(callbacksRecorded[0] && callbacksRecorded[1] && callbacksRecorded[2]){
-          done();
-        }
-      }
-      questionGenerator.when(
-        {
-          topicClass : "testClass",
-          topicId    : 1
-        },function(callback){
-          callback(null,"question1");
-        }
-      );
-      questionGenerator.when(
-        {
-          topicClass : "testClass",
-          topicId    : 2
-        },function(callback){
-          callback(null,"question2");
-        }
-      );
-      questionGenerator.when(
-        {
-          topicClass : "testClass",
-          topicId    : 3
-        },function(callback){
-          callback(null,"question3");
-        }
-      );
-      questionGenerator.generate(
-        {
-          topicClass : "testClass",
-          topicId    : 2
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question2");      
-          recordCallback(0);
-        }
-      );
-      questionGenerator.generate(
-        {
-          topicClass : "testClass",
-          topicId    : 3
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question3");      
-          recordCallback(1);
-        }
-      );    
-      questionGenerator.generate(
-        {
-          topicClass : "testClass",
-          topicId    : 1
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question1");      
-          recordCallback(2);
-        }
-      );        
-    });
-
-    it('Calls correct function for each class',function(done){
-      var callbacksRecorded=[false,false,false];
-      var recordCallback=function(index){
-        callbacksRecorded[index]=true;
-        if(callbacksRecorded[0] && callbacksRecorded[1] && callbacksRecorded[2]){
-          done();
-        }
-      }
-      questionGenerator.when(
-        {
-          topicClass : "testClass1",
-          topicId    : 1
-        },function(callback){
-          callback(null,"question1");
-        }
-      );
-      questionGenerator.when(
-        {
-          topicClass : "testClass2",
-          topicId    : 1
-        },function(callback){
-          callback(null,"question2");
-        }
-      );
-      questionGenerator.when(
-        {
-          topicClass : "testClass3",
-          topicId    : 1
-        },function(callback){
-          callback(null,"question3");
-        }
-      );
-      questionGenerator.generate(
-        {
-          topicClass : "testClass2",
-          topicId    : 1
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question2");      
-          recordCallback(0);
-        }
-      );
-      questionGenerator.generate(
-        {
-          topicClass : "testClass3",
-          topicId    : 1
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question3");      
-          recordCallback(1);
-        }
-      );    
-      questionGenerator.generate(
-        {
-          topicClass : "testClass1",
-          topicId    : 1
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question1");      
-          recordCallback(2);
-        }
-      );        
-    });
-
-    
-    it('Allows chaining for registration methods',function(done){
-      var callbacksRecorded=[false,false,false];
-      var recordCallback=function(index){
-        callbacksRecorded[index]=true;
-        if(callbacksRecorded[0] && callbacksRecorded[1] && callbacksRecorded[2]){
-          done();
-        }
-      }
-      questionGenerator.when(
-        {
-          topicClass : "testClass",
-          topicId    : 1
-        },function(callback){
-          callback(null,"question1");
-        })
-      .when(
-        {
-          topicClass : "testClass",
-          topicId    : 2
-        },function(callback){
-          callback(null,"question2");
-        })
-      .when(
-        {
-          topicClass : "testClass",
-          topicId    : 3
-        },function(callback){
-          callback(null,"question3");
-        });
-      
-      questionGenerator.generate(
-        {
-          topicClass : "testClass",
-          topicId    : 2
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question2");      
-          recordCallback(0);
-        });
-    
-      questionGenerator.generate(
-        {
-          topicClass : "testClass",
-          topicId    : 3
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question3");      
-          recordCallback(1);
-        });    
-
-      questionGenerator.generate(
-        {
-          topicClass : "testClass",
-          topicId    : 1
-        },function(err,question){
-          expect(err).toBeNull();
-          expect(question).toEqual("question1");      
-          recordCallback(2);
-        });
-    });
-    
-  });  
   
-  describe('generator',function(){
+  
+  it('Additional options are passed to the generator function',function(done){
+    var topic = {
+      //TODO use - but how can we expect the callback to be async? callback:jasmine.createSpy('generateQuestion')
+      generateQuestion:function(callback,options){
+        callback(null,"question1 with additional option " + options.testOption);
+      }
+    }
+    mockTopicRegistry.getTopic=function(id,callback){        
+      callback(null,topic);
+    };
+    mockTopicRegistry.getTopics=function(filter,callback){        
+      callback(null,[topic]);
+    };
+    questionGenerator.generate({
+      topicClass : "testClass",
+      topicId    : 1,
+      testOption : "foo"
+    },function(err,question){
+      expect(err).toBeNull();
+      expect(question).toEqual("question1 with additional option foo");            
+      done();
+    });
+    
+  });
+  
+  describe('default generator',function(){
+    
+    beforeEach(function(){//TODO refactor, use jasmine.mock or something here?
+      mockTopicRegistry.ERR_UNKNOWN_ID={reason:"ERR_UNKNOWN_ID"};
+      mockTopicRegistry.getTopics = function(filter,callback){
+        callback(this.ERR_UNKNOWN_ID,null);
+      }
+    });
+    
     it('registers default generator function',function(done){
-      questionGenerator.otherwise(function(questionSpec,callback){
+      questionGenerator.setDefault(function(questionSpec,callback){
         callback(null,"placeholder for topic " + questionSpec.topicId);
       });
       
@@ -246,7 +76,7 @@ describe('GreatMath.question-generator module', function() {
         }
       );
     });
-    
+  
     it('throws exception for unkown topic id if no default generator is registered',function(){
       var questionSpec = {
         topicClass : "mentalStrategies",
@@ -257,95 +87,16 @@ describe('GreatMath.question-generator module', function() {
       });
     });
 
-    it('Additional options are passed to the generator function',function(){
-      questionGenerator.when({
-        topicClass : "testClass",
-        topicId    : 1
-      },function(callback,options){
-        callback(null,"question1 with additional option " + options.testOption);
-      });
-      
-      questionGenerator.generate({
-        topicClass : "testClass",
-        topicId    : 1,
-        testOption : "foo"
-      },function(err,question){
-        expect(err).toBeNull();
-        expect(question).toEqual("question1 with additional option foo");            
-      });
-      
-    });
-    
   });  
-  
-  
+   
+  //Not implemented yet - do we need this?
   xit('registers default generator function on a per topic class basis',function(done){});
-  
-  
-  xit('throws exception for topic class if no default generator is registered',function(){});
-  
-  
-  describe('topic list',function(){
-    beforeEach(function(){
-      questionGenerator.when(
-        {
-          topicClass       : "testClass",
-          topicId          : 1,
-          topicDescription : "topic 1 description"
-        },function(callback,options){
-          callback(null,"question1");
-        }
-      )
-      .when(
-        {
-          topicClass : "testClass",
-          topicId    : 2,
-          topicDescription : "topic 2 description"
-        },function(callback,options){
-          callback(null,"question2");
-        }
-      )
-      .when(
-        {
-          topicClass : "testClass",
-          topicId    : 3,
-          topicDescription : "topic 3 description"
-        },function(callback,options){
-          callback(null,"question3");
-        }
-      );
-    });
     
-    it('provides a list of all registered topics',function(done){
-      questionGenerator.getTopics(function(topics){
-        expect(topics.length).toBe(3);
-        done();
-      });
-    });
-
-    it('topics contain correct description',function(done){
-      questionGenerator.getTopics(function(topics){
-        expect(topics[1].description).toEqual('topic 2 description');
-        done();
-      });
-    });
-
-    it('topics contain correct id',function(done){
-      questionGenerator.getTopics(function(topics){
-        expect(topics[1].id).toEqual(2);
-        done();
-      });
-    });     
-
-    xit('getTopics is async');
-    
-    
-  });
-  
-  
   xit('allows default function to be async',function(){});
   
+  //DEBT:implemented but not tested
   xit('calls "no default function" error async',function(){});
   
-  xit('duplicate registrations causes error');
+  
+  
 });
