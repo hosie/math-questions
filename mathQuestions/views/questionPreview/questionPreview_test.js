d72153b28b24ba4990efd88ad74e1f9f54035b8f
hosie <jhosie@uk.ibm.com>
2015-08-04 10:50:02 +0100

    update tests for hack

--- mathQuestions/views/questionPreview/questionPreview_test.js
+++ mathQuestions/views/questionPreview/questionPreview_test.js
@@ -72,7 +72,7 @@ describe('QuestionPreviewController',function(){
           expect(mockQuestionGenerator.generate).toHaveBeenCalledWith(
             { 
               class : "mentalStrategies",
-              topicId    : 42
+              topicId    : 41
             },
             jasmine.any(Function)
           );
